4e4d916096410f0944c4055a9401ef50bac2881e
Vain_Kane <vidueduo@gmail.com>
2026-07-19 18:10:21 +0700

    19/07/2026

--- _CVPOI/Mixed/t9.cpp
+++ _CVPOI/Mixed/t9.cpp
@@ -49,6 +49,8 @@ int main()
 
     freopen(name".inp", "r", stdin);
     freopen(name".out", "w", stdout);
+    freopen(name".inp", "r", stdin);
+    freopen(name".out", "w", stdout);
 
     cin >> n >> k;
     FOR(i, 1, n) cin >> x[i] >> y[i];
