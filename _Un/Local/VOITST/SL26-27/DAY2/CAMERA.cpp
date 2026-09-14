3f9987c76cf74917eb034644c19ece510d4a9641
Vain_Kane <vidueduo@gmail.com>
2026-09-14 11:38:48 +0700

    14/09/2026

---
+++ _Un/Local/VOITST/SL26-27/DAY2/CAMERA.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define FORD(i, b, a) for (int i = (b), _a = (a); i >= _a; i--)
+#define REP(i, n) for (int i = 0, _n = (n); i < _n; i++)
+#define BIT(i, x) (((x) >> (i)) & 1)
+#define MK(i) (1LL << (i))
+#define all(v) v.begin(), v.end()
+#define sz(v) ((int)v.size())
+#define F first
+#define S second
+#define name ""
+
+using ll = long long;
+using ii = pair<int, int>;
+
+template <class T> bool maxi(T &x, T const &y) { return x < y ? x = y, 1 : 0; }
+template <class T> bool mini(T &x, T const &y) { return x > y ? x = y, 1 : 0; }
+
+int const N = 2e5 + 5;
+int const oo = 1e9 + 9;
+
+int n;
+vector<int> adj[N];
+
+int dp[N][3];
+
+void DFS(int u, int p)
+{
+    dp[u][1] = 1;
+    int delta = oo;
+
+    for (auto &v : adj[u]) if (v != p)
+    {
+        DFS(v, u);
+
+        if (dp[u][0] < oo) dp[u][0] += dp[v][2];
+        if (dp[u][1] < oo) dp[u][1] += min({dp[v][0], dp[v][1], dp[v][2]});
+        if (dp[u][2] < oo) dp[u][2] += min(dp[v][1], dp[v][2]);
+ 
+        mini(delta, dp[v][1] - min(dp[v][1], dp[v][2]));
+    }
+
+    if (dp[u][2] < oo) dp[u][2] += delta;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n;
+    FOR(i, 2, n)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    FOR(u, 2, n) if (sz(adj[u]) == 1) dp[u][2] = oo;
+
+    DFS(1, -1);
+    cout << min(dp[1][1], dp[1][2]);
+
+    return 0;
+}
