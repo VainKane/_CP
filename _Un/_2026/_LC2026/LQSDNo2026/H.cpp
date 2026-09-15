faf615da3628f7c3314b808df7a0690e1a94b4cb
Vain_Kane <vidueduo@gmail.com>
2026-09-15 11:37:37 +0700

    15/09/2026

--- _Un/_2026/_LC2026/LQSDNo2026/H.cpp
+++ _Un/_2026/_LC2026/LQSDNo2026/H.cpp
@@ -19,10 +19,64 @@ using ii = pair<int, int>;
 template <class T> bool maxi(T &x, T const &y) { return x < y ? x = y, 1 : 0; }
 template <class T> bool mini(T &x, T const &y) { return x > y ? x = y, 1 : 0; }
 
+int const N = 3009;
+
+int n, q;
+bool a[N][N];
+
+int f1[N][N][2], f2[N][N][2];
+
+bool CheckF1(int x, int y, int h, int c)
+{
+    if (f1[x][y][c] > h)
+    {
+        FOR(k, 1, 2) if (min(f1[x][y + k][c ^ 1], f1[x + k][y][c ^ 1]) <= h - k) return false;
+        return true;
+    }
+
+    return false;
+}
+
+bool CheckF2(int x, int y, int h, int c)
+{
+    if (f2[x][y + h][c] > h)
+    {
+        FOR(k, 1, 2) if (min(f2[x][y + h - k][c ^ 1], f2[x + k][y + h][c ^ 1]) <= h - k) return false;
+        return true;
+    }
+
+    return false;
+}
+
+bool Check(int x, int y, int h)
+{
+    REP(c, 2) if (CheckF1(x, y, h, c) || CheckF2(x, y, h, c)) return true;
+    return false;
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
+
+    cin >> n >> q;
+    FOR(i, 1, n) FOR(j, 1, n)
+    {
+        char ch; cin >> ch;
+        a[i][j] = ch - '0';
+    }
+
+    FORD(i, n, 1) FORD(j, n, 1) REP(k, 2) f1[i][j][k] = a[i][j] == k ? f1[i + 1][j + 1][k] + 1 : 0;
+    FORD(i, n, 1) FOR(j, 1, n) REP(k, 2) f2[i][j][k] = a[i][j] == k ? f2[i + 1][j - 1][k] + 1 : 0;
+
+    while (q--)
+    {
+        int h, x, y;
+        cin >> h >> x >> y;
+        x++, y++;
+
+        cout << Check(x, y, h) << '\n';
+    }
 
     return 0;
 }
---
+++ __VNOJ/_VOI/VOI17/METRO.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define FORD(i, b, a) for (int i = (b), _a = (a); i >= _a; i--)
+#define REP(i, n) for (int i = 0, _n = (n); i < _n; i++)
+#define BIT(i, x) (((x) >> (i)) & 1)
+#define MK(i) (1LL << (i))
+#define all(v) v.begin(), v.end()
+#define sz(v) ((int)v.size())
+#define F first
+#define S second
+#define name ""
+
+using ll = long long;
+using ii = pair<int, int>;
+
+template <class T> bool maxi(T &x, T const &y) { return x < y ? x = y, 1 : 0; }
+template <class T> bool mini(T &x, T const &y) { return x > y ? x = y, 1 : 0; }
+
+int const N = 1e5 + 5;
+ll const oo = 1e18 + 9;
+
+struct Edge { int v, w, id; };
+
+int n, m, s, t, delta;
+
+vector<Edge> adj[N];
+ll d[N], res[N];
+
+void Dijkstra(int s)
+{
+    memset(d, 0x3f, sizeof d);
+    memset(res, 0x3f, sizeof res);
+    d[s] = res[s] = 0;
+
+    priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<pair<ll, int>>> pq;
+    pq.push({d[s], s});
+
+    while (!pq.empty())
+    {
+        int u = pq.top().S;
+        ll du = pq.top().F;
+        pq.pop();
+
+        if (du > d[u]) continue;
+
+        for (auto &e : adj[u])
+        {
+            int v = e.v;
+            mini(res[v], d[u] + e.w + e.id * (u != s));
+            if (mini(d[v], d[u] + e.w + (delta + u != s) * e.id)) pq.push({d[v], v});
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> n >> m >> s >> t >> delta;
+    FOR(i, 1, m)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w, i});
+    }
+
+    Dijkstra(s);
+    cout << (res[t] < oo ? res[t] : -1);
+
+    return 0;
+}
